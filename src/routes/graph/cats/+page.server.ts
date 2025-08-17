8f5614b2b35bc19a27012b773d932431128da8bf
Oliver <oholl@ethz.ch>
2025-08-17 18:19:29 +0200

    up

--- src/routes/graph/cats/+page.server.ts
+++ src/routes/graph/cats/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from './$types';
-import graphData from '$lib/../../../static/jsons/category_graph_data_optimized.json';
-import categoryStats from '$lib/../../../static/jsons/categories.json';
+import graphData from '$lib/files/category_graph_data_optimized.json';
+import categoryStats from '$lib/files/categories.json';
 
 export const load: PageServerLoad = async () => {
     // Both files are bundled at build time
